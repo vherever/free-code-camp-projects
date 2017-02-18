ee7e2ef4a65abdde91e7a0055f737e0876f2be2c
vherever <vherever@gmail.com>
2017-02-19 01:48:08 +0200

    Reverse a String

---
+++ Basic Algorithm Scripting/Reverse_a_String.js
@@ -0,0 +1,24 @@
+/**
+ * Reverse a String
+ *
+ * Reverse the provided string.
+
+ * You may need to turn the string into an array before you can reverse it.
+
+ * Your result must be a string.
+**/
+
+function reverseString(str) {
+    var newStr = "";
+    for(var i = str.length - 1; i >= 0; i --) {
+        newStr += str[i];
+    }
+    return newStr;
+}
+
+// A bit easier way
+function reverseString2(str) {
+    return str.split("").reverse().join("");
+}
+
+console.log(reverseString("hello")); // returns olleh
