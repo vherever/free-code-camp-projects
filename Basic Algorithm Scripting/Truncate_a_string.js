316da5ca222e95c69a064dc9345366fee78b268c
Vladimir <vromanyuk@it7.com.ua>
2017-03-22 14:32:19 +0200

    Truncate a string

--- Basic Algorithm Scripting/Truncate_a_string.js
+++ Basic Algorithm Scripting/Truncate_a_string.js
@@ -14,7 +14,13 @@
 
 function truncateString(str, num) {
     // Clear out that junk in your trunk
-    return str;
+    if(num <= 3) {
+        return str.slice(0, num) + '...';
+    } else if(num >= str.length) {
+        return str;
+    } else {
+        return str.slice(0, num - 3) + '...';
+    }
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 11);
