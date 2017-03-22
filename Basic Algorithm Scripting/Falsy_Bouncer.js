60ce9365d25e1d00de12d3c59ff682fa4ded483c
vherever <vherever@gmail.com>
2017-03-22 23:46:16 +0200

    Falsy Bouncer

--- Basic Algorithm Scripting/Falsy_Bouncer.js
+++ Basic Algorithm Scripting/Falsy_Bouncer.js
@@ -10,7 +10,9 @@
 
 function bouncer(arr) {
     // Don't show a false ID to this bouncer.
-    return arr;
+    return arr.filter(function(val) {
+        return !!val;
+    });
 }
 
 bouncer([7, "ate", "", false, 9]);
