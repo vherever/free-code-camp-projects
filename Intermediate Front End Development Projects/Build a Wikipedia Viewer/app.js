67a4584c6197f5df56ab0de797ad3bf66239083d
vherever <vherever@gmail.com>
2017-04-07 12:35:09 +0300

    Wikipedia Viewer

--- Intermediate Front End Development Projects/Build a Wikipedia Viewer/app.js
+++ Intermediate Front End Development Projects/Build a Wikipedia Viewer/app.js
@@ -12,35 +12,34 @@ app.controller('MainController', function ($scope, $http) {
             search: $('#search'),
             result: $('#results')
         },
-        api: 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=',
+        apiUrl: 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=',
         cb: '&callback=JSON_CALLBACK',
-        pageId: 'https://en.wikipedia.org/?curid='
+        pageUrl: 'https://en.wikipedia.org/?curid='
     };
+
+    function cropImage(imgUrl) {
+        if (imgUrl.indexOf("50px-") >= 0) {
+            return imgUrl.replace('50px-', '100px-');
+        } else {
+            return imgUrl.replace('36px-', '75px-');
+        }
+    }
 
     $scope.search = function () {
         $scope.results = [];
         var searchWord = o.selectors.search.val();
 
-        // $http.get(o.api + searchWord + o.cb)
-        //     .then(function (res) {
-        //         console.log('res', res);
-        //     }, function () {
-        //
-        //     })
-
-        $http.jsonp(o.api + searchWord + o.cb)
-            .success(function(res) {
-                angular.forEach(res.pages, function (i) {
+        $http.jsonp(o.apiUrl + searchWord + o.cb)
+            .success(function (res) {
+                angular.forEach(res.query.pages, function (i) {
                     $scope.results.push({
                         title: i.title,
                         content: i.extract,
-                        pageId: i.pageid,
-                        thumbnail: i.thumbnail.source
-                    });
+                        pageUrl: o.pageUrl + i.pageid,
+                        imageUrl: i.thumbnail ? cropImage(i.thumbnail.source) : ''
+                    })
                 });
             })
-
-
     };
 
 
